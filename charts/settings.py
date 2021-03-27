cfab7f8633cb2925bba4a8af49f25510580f44a8
kothawleprem <kothawleprem@gmail.com>
2021-03-27 21:43:02 +0530

    Backend Done

--- charts/settings.py
+++ charts/settings.py
@@ -122,10 +122,9 @@ USE_TZ = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-EMAIL_HOST_USER = "chartbayindia@gmail.com"
-EMAIL_HOST_PASSWORD = "mops@123"
+EMAIL_HOST_USER = "Your email"
+EMAIL_HOST_PASSWORD = "Your password"
+
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
