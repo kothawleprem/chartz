7329e619f84ad73b1ddc1f07976656b5afcad028
kothawleprem <kothawleprem@gmail.com>
2021-03-27 21:30:25 +0530

    Checking

--- charts/settings.py
+++ charts/settings.py
@@ -122,10 +122,10 @@ USE_TZ = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-EMAIL_HOST_USER = "testalphapsk@gmail.com"
-EMAIL_HOST_PASSWORD = "alphapsk123"
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+# EMAIL_HOST_USER = "testalphapsk@gmail.com"
+# EMAIL_HOST_PASSWORD = "alphapsk123"
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
