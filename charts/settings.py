7d736f36d1fb0539ae3f8103e3c1e968001563af
kothawleprem <kothawleprem@gmail.com>
2021-03-28 11:06:56 +0530

    Completed

--- charts/settings.py
+++ charts/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "chartbayindia@gmail.com"
-EMAIL_HOST_PASSWORD = "mops@123"
+EMAIL_HOST_USER = "Your E-mail"
+EMAIL_HOST_PASSWORD = "Your Password"
 
 
 
