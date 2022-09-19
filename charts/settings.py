5b496ad06c6a5466684405fbe2ded7cf08304ce3
kothawleprem <kothawleprem@gmail.com>
2022-09-19 23:08:04 +0530

    Update minor bugs

--- charts/settings.py
+++ charts/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ne@48ujd9eqemm*^(p5fmz^v6nckd2_u$fn(re^p!k!iszjob='
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -119,15 +119,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "Your E-mail"
-EMAIL_HOST_PASSWORD = "Your Password"
-
-
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_USE_TLS = True
 
 
 # Static files (CSS, JavaScript, Images)
